eece7300ad0233a3b0049743282c32c438b45f7f
Sreenithy Thayanithy <94304445+sreenithyt@users.noreply.github.com>
2025-05-29 07:05:20 +0530

    Update router_top_tb.v

--- TB/router_top_tb.v
+++ TB/router_top_tb.v
@@ -1,3 +1,11 @@
+/***************************************************************************************************
+
+NAME        :  Sreenithy T
+FILENAME    :  router_top_tb.v
+DATE        :  03/05/2025
+DESCRIPTION :  ROUTER - TOP testbench
+
+****************************************************************************************************/
 module router_top_tb();
 
 reg clk, resetn, read_enb_0, read_enb_1, read_enb_2, packet_valid;
@@ -6,21 +14,12 @@ wire [7:0]data_out_0, data_out_1, data_out_2;
 wire vld_out_0, vld_out_1, vld_out_2, err, busy;
 integer i;
 
-router_top DUT(.clk(clk),
-			   .resetn(resetn),
-			   .read_enb_0(read_enb_0),
-			   .read_enb_1(read_enb_1),
-			   .read_enb_2(read_enb_2),
-			   .packet_valid(packet_valid),
-			   .datain(datain),
-			   .data_out_0(data_out_0),
-			   .data_out_1(data_out_1),
-			   .data_out_2(data_out_2),
-			   .vldout_0(vld_out_0),
-			   .vldout_1(vld_out_1),
-			   .vldout_2(vld_out_2),
-			   .err(err),
-			   .busy(busy) );			   
+  router_top DUT(.clock(clk), .resetn(resetn), 
+                 .read_enb_0(read_enb_0), .read_enb_1(read_enb_1), .read_enb_2(read_enb_2), 
+                 .pkt_valid(packet_valid), .data_in(datain), 
+                 .data_out_0(data_out_0), .data_out_1(data_out_1), .data_out_2(data_out_2),
+                 .vld_out_0(vld_out_0), .vld_out_1(vld_out_1), .vld_out_2(vld_out_2), 
+                 .err(err),.busy(busy) );			   
 			   
 //clock generation
 
@@ -34,52 +33,24 @@ initial
 	
 	task reset;
 		begin
+		    @(negedge clk)
 			resetn=1'b0;
-			{read_enb_0, read_enb_1, read_enb_2, packet_valid, datain}=0;
-			#10;
+			@(negedge clk)
 			resetn=1'b1;
 		end
 	endtask
 	
-	task pktm_gen_8;	// packet generation payload 8
+	task initialize;
+	    begin
+		   resetn = 1'b1;
+		   {read_enb_0, read_enb_1, read_enb_2, packet_valid}=0;
+		end
+    endtask
+		
+	
+	task pktm_gen_5;	// packet generation payload 5
 			reg [7:0]header, payload_data, parity;
 			reg [8:0]payloadlen;
-			
-			begin
-				parity=0;
-				wait(!busy)
-				begin
-				@(negedge clk);
-				payloadlen=8;
-				packet_valid=1'b1;
-				header={payloadlen,2'b10};
-				datain=header;
-				parity=parity^datain;
-				end
-				@(negedge clk);
-							
-				for(i=0;i<payloadlen;i=i+1)
-					begin
-					wait(!busy)				
-					@(negedge clk);
-					payload_data={$random}%256;
-					datain=payload_data;
-					parity=parity^datain;				
-					end					
-								
-				wait(!busy)				
-					@(negedge clk);
-					packet_valid=0;				
-					datain=parity;
-					repeat(30)
-			@(negedge clk);
-			read_enb_1=1'b1;
-			end
-endtask
-	
-	task pktm_gen_5;	// packet generation payload 8
-			reg [7:0]header, payload_data, parity;
-			reg [4:0]payloadlen;
 			
 			begin
 				parity=0;
@@ -96,32 +67,141 @@ endtask
 							
 				for(i=0;i<payloadlen;i=i+1)
 					begin
-					wait(!busy)				
+					wait(!busy)
+                    begin  
 					@(negedge clk);
 					payload_data={$random}%256;
 					datain=payload_data;
-					parity=parity^datain;				
+					parity=parity^datain;
+                    end  
 					end					
 								
-				wait(!busy)				
+              wait(!busy)
+                    begin
 					@(negedge clk);
 					packet_valid=0;				
 					datain=parity;
-					repeat(30)
+                    end  
+              repeat(2)
 			@(negedge clk);
 			read_enb_2=1'b1;
+              
+              wait(DUT.FIFO_2.empty)
+           @(negedge clk)
+           read_enb_2=0;  
+			end
+      
+endtask
+	
+	task pktm_gen_14;	// packet generation payload 14
+			reg [7:0]header, payload_data, parity;
+			reg [8:0]payloadlen;
+			
+			begin
+				parity=0;
+				wait(!busy)
+				begin
+				@(negedge clk);
+				payloadlen=14;
+				packet_valid=1'b1;
+				header={payloadlen,2'b01};
+				datain=header;
+				parity=parity^datain;
+				end
+				@(negedge clk);
+							
+				for(i=0;i<payloadlen;i=i+1)
+					begin
+					wait(!busy)
+                    begin  
+					@(negedge clk);
+					payload_data={$random}%256;
+					datain=payload_data;
+					parity=parity^datain;
+                    end  
+					end					
+								
+              wait(!busy)
+                    begin
+					@(negedge clk);
+					packet_valid=0;				
+					datain=parity;
+                    end  
+              repeat(2)
+			@(negedge clk);
+			read_enb_1=1'b1;
+              
+              wait(DUT.FIFO_1.empty)
+           @(negedge clk)
+           read_enb_1=0;  
 			end
 endtask
+
+	task pktm_gen_16;	// packet generation payload 16
+			reg [7:0]header, payload_data, parity;
+			reg [8:0]payloadlen;
+			
+			begin
+				parity=0;
+				wait(!busy)
+				begin
+				@(negedge clk);
+				payloadlen=16;
+				packet_valid=1'b1;
+				header={payloadlen,2'b00};
+				datain=header;
+				parity=parity^datain;
+				end
+				@(negedge clk);
+							
+				for(i=0;i<payloadlen;i=i+1)
+					begin
+					wait(!busy)
+                    begin  
+					@(negedge clk);
+					payload_data={$random}%256;
+					datain=payload_data;
+					parity=parity^datain;
+                    end  
+					end					
+								
+              wait(!busy)
+                    begin
+					@(negedge clk);
+					packet_valid=0;				
+					datain=parity;
+                    end  
+              repeat(2)
+			@(negedge clk);
+			read_enb_0=1'b1;
+              
+              wait(DUT.FIFO_0.empty)
+           @(negedge clk)
+           read_enb_0=0;  
+			end
+endtask
+
+
 	
 	initial
 		begin
+		    initialize;
 			reset;
 			#10;
-			pktm_gen_8;
 			pktm_gen_5;
-			#1000;
+            #100;
+            //reset;
+			pktm_gen_14;
+			#100;
+			pktm_gen_16;
+			#700;
 			$finish;
 		end
 		
+  initial
+    begin
+      $dumpfile("router_top_tb.vcd");
+      $dumpvars;
+    end
 		
 endmodule
