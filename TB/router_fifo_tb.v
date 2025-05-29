86a772de0c1d9ada7aeea6cfd53d801b3cfa7408
Sreenithy Thayanithy <94304445+sreenithyt@users.noreply.github.com>
2025-05-29 07:02:21 +0530

    Update router_fifo_tb.v

--- TB/router_fifo_tb.v
+++ TB/router_fifo_tb.v
@@ -1,53 +1,122 @@
+/***************************************************************************************************
+
+NAME        :  Sreenithy T
+FILENAME    :  router_fifo_tb.v
+DATE        :  03/05/2025
+DESCRIPTION :  ROUTER - FIFO testbench
+
+****************************************************************************************************/
+
 module router_fifo_tb();
-//parameter DELAY=10;
-//parameter DEPTH=16, WIDTH=9, ADD_SIZE=5;
-reg clk, resetn, write_enb, read_enb, lfd_state, soft_reset;
+
+reg clock,resetn,soft_reset,write_enb,read_enb,lfd_state;
 reg [7:0]data_in;
-wire full, empty;	
+wire full,empty;
 wire [7:0]data_out;
 
+parameter period=10;
+reg[7:0]header,parity;
 
-router_fifo DUT    (.clk(clk),
-					.resetn(resetn),
-					.soft_reset(soft_reset),
-					.write_enb(write_enb),
-					.read_enb(read_enb),
-					.lfd_state(lfd_state),
-					.datain(data_in),
-					.full(full),
-					.empty(empty),
-					.dataout(data_out));
+reg[1:0]addr;
+integer i;
 
-	//clock generation
-	initial begin
-	clk = 1;
-	forever 
-	#5 clk=~clk;
-	end
-	
+router_fifo DUT(clock,resetn,soft_reset,write_enb,read_enb,lfd_state,data_in,full,empty,data_out);
+
+//-------------------------- Clock Generation Block ----------------------------------
+
+initial
+begin
+  clock=1'b0;
+  forever #(period/2) clock = ~clock;
+end
+
+//----------------------------- Reset Task Block --------------------------------------
+
+task rst();
+  begin
+    @(negedge clock)
+    resetn=1'b0;
+    @(negedge clock)
+    resetn=1'b1;
+  end
+endtask
+
+task soft_rst();
+  begin
+      @(negedge clock)
+      soft_reset=1'b1;
+      @(negedge clock)
+      soft_reset=1'b0;
+  end
+endtask
+
+//---------------------------- Initialization Task Block ------------------------------- 
+
+task initialize();
+  begin
+    write_enb=1'b0;
+    soft_reset=1'b0;
+    read_enb=1'b0;
+    data_in=0;
+    lfd_state=1'b0;
+  end
+endtask
+
+//--------------------------- Read & Write Task Block ----------------------------------
 
 
-initial 
-	begin
-	resetn=1'b0;
-	#10;
-	resetn=1'b1;
-	soft_reset=1'b0;
-	lfd_state=1'b1;
-	write_enb=1'b1;
-	#10;
-	repeat(17)
-		begin
-			data_in=$random;
-			#10;
-		end
-	write_enb=1'b0;
-	#5;
-	
-	read_enb=1'b1;
-	#100;
-	
- 
-	$finish; 
-	end 
+
+task pkt_gen;
+	reg[7:0]payload_data;
+	reg[5:0]payload_len;
+     begin
+     @(negedge clock);
+     payload_len=6'd14;
+     addr=2'b01;
+     header={payload_len,addr};
+     data_in=header;
+     lfd_state=1'b1; write_enb=1;
+     
+     for(i=0;i<payload_len;i=i+1)
+
+     begin
+    
+     @(negedge clock);
+     lfd_state = 0;
+     payload_data={$random}%256;
+     data_in=payload_data;
+     end
+   
+     @(negedge clock);
+     parity={$random}%256;
+     data_in=parity;
+     end
+
+endtask
+
+initial
+begin
+rst();
+
+soft_rst;
+pkt_gen;
+
+repeat(2)
+@(negedge clock);
+read_enb=1;
+write_enb=0;
+@(negedge clock)
+wait(empty)
+  @(negedge clock)
+read_enb=0;
+
+#100 $finish;
+end
+
+initial
+begin
+$dumpfile("router_fifo_tb.vcd");
+$dumpvars();
+end
+
 endmodule
